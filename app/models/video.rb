57f0011d6b381576b8f00a04605e18859c8c64be
Manh Vu <vdmanh107@gmail.com>
2023-10-01 23:18:07 +0700

    draft user, video, devise

---
+++ app/models/video.rb
@@ -0,0 +1,23 @@
+class Video < ApplicationRecord
+  belongs_to :user
+end
+
+# == Schema Information
+#
+# Table name: videos
+#
+#  id         :bigint           not null, primary key
+#  title      :string
+#  url        :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_videos_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
