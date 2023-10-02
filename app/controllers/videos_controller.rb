1425b0a00ff0f781226a01accbb441606e58ba5e
Manh Vu <vdmanh107@gmail.com>
2023-10-02 23:54:55 +0700

    add basic video sharing feature

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,6 @@
 class VideosController < ApplicationController
+  before_action :authenticate_user!, except: %i(index)
+
   # GET /videos
   def index
     @videos = Video.all
@@ -9,5 +11,21 @@ class VideosController < ApplicationController
   end
 
   def create
+    @video = Video.new(user: current_user, url: create_params[:url])
+
+    if @video.save
+      flash[:success] = "Movie shared"
+      redirect_to new_video_path
+    else
+      @flash_errors = @video.errors.full_messages
+      flash[:danger] = "Movie failed to share, please try again"
+      render :new
+    end
+  end
+
+  private
+
+  def create_params
+    params.require(:video).permit(:url)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_01_182054) do
+ActiveRecord::Schema.define(version: 2023_10_02_164610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2023_10_01_182054) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "like_count", default: 0
     t.integer "dislike_count", default: 0
+    t.string "youtube_video_id"
+    t.text "description"
     t.index ["user_id"], name: "index_videos_on_user_id"
   end
 
